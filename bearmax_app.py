53d76eb7f99cd5353636ac9563d167067f42a58d
KartikKapur <kartik@airbears2-10-142-169-89.airbears2.1918.berkeley.edu>
2016-11-12 02:04:00 -0800

    plssss

--- bearmax_app.py
+++ bearmax_app.py
@@ -3,6 +3,7 @@ from flask import request, redirect
 import json
 import requests
 import urllib
+from webob import Response
 
 FB_APP_TOKEN = 'EAAQ7z3BxdYgBAHGNIM6phWb4mH0vDCfsaQaY5rxoN4ZCzZBaKmheZCsYZAkLZCB5XLmcUKkby9N5ncCPIH58swZCp5jRhTTnEQ9aaDttl4eVYUMp8h3x954vUQ6SsX5JOPeEZAhGkdl3ot5jfy8UtgZBDIXWdkOyk51Q13C3ZBoAC2QZDZD'
 FB_ENDPOINT = 'https://graph.facebook.com/v2.6/me/{0}'
@@ -27,7 +28,7 @@ def webhook():
 		sender_id = event['sender']['id']
 		send_FB_message(sender_id, 'bearmax')
 
-
+    return Response()
 
 
 def send_FB_message(sender_id, message):
