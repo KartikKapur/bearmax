ad49cc9dfe6ea9d7b7b148988ccd74947aa0f46c
Ajay Raj <ajay@airbears2-10-142-139-252.airbears2.1918.berkeley.edu>
2016-11-12 05:28:12 -0800

    finish watson

--- watson.py
+++ watson.py
@@ -2,19 +2,40 @@ import json
 from watson_developer_cloud import NaturalLanguageClassifierV1
 import sys
 
-if __name__ == '__main__':
+USERNAME = '9bb44fae-5d4f-4f55-a024-6278ae14c655'
+PASSWORD = 'ITch7aNcuaa2'
 
-    natural_language_classifier = NaturalLanguageClassifierV1(username='9bb44fae-5d4f-4f55-a024-6278ae14c655', password='ITch7aNcuaa2')
+CLASSES_PATH = 'resources/classes.csv'
+
+def get_symptoms(statement, natural_language_classifier, instance_id):
+
+    status = natural_language_classifier.status(instance_id)
+    print(json.dumps(status, indent=2))
+
+    if status['status'] == 'Available':
+        classes = natural_language_classifier.classify(instance_id, statement)
+        print(json.dumps(classes, indent=2))
+        return classes['top_class'], classes['classes']
+    return None, []
+
+def init_nat_lang_classifier(initialized=False):
+
+    natural_language_classifier = NaturalLanguageClassifierV1(username=USERNAME, password=PASSWORD)
 
     classifiers = natural_language_classifier.list()
     print(json.dumps(classifiers, indent=2))
 
-    with open('resources/classes.csv', 'rb') as training_data:
-        print(json.dumps(natural_language_classifier.create(training_data=training_data, name='symptoms'), indent=2))
+    if initialized and classifiers:
+        return natural_language_classifier, classifiers['classifiers'][0]['classifier_id']
 
-    status = natural_language_classifier.status('e82f62x108-nlc-5198')
-    print(json.dumps(status, indent=2))
+    if not initialized:
+        with open(CLASSES_PATH, 'rb') as training_data:
+            response = natural_language_classifier.create(training_data=training_data, name='symptoms'),
+            print(json.dumps(response, indent=2))
+            return natural_language_classifier, response[0]['classifier_id']
 
-    if status['status'] == 'Available':
-        classes = natural_language_classifier.classify('e82f62x108-nlc-5198', sys.argv[1])
-        print(json.dumps(classes, indent=2))
+    return
+
+if __name__ == '__main__':
+    natural_language_classifier, instance_id = init_nat_lang_classifier(True)
+    print(get_symptoms(sys.argv[1], natural_language_classifier, instance_id))
