7036b173f853ede2f8fc1980d7056be5915a05c2
KartikKapur <kartik@calvisitor-10-105-153-53.calvisitor.1918.berkeley.edu>
2016-11-12 16:34:09 -0800

    demographics

---
+++ facebook.py
@@ -0,0 +1,17 @@
+import facebook
+token = 'EAAQ7z3BxdYgBAHGNIM6phWb4mH0vDCfsaQaY5rxoN4ZCzZBaKmheZCsYZAkLZCB5XLmcUKkby9N5ncCPIH58swZCp5jRhTTnEQ9aaDttl4eVYUMp8h3x954vUQ6SsX5JOPeEZAhGkdl3ot5jfy8UtgZBDIXWdkOyk51Q13C3ZBoAC2QZDZD'
+graph = facebook.GRAPHAPI(token)
+
+class Userdetails:
+    def __init__(self, gender, birthday,location):
+        self.gender = gender
+        self.birthday= birthday
+        self.location = location
+    def get_demographics(self):
+        return gender, birthdate
+
+gender = graph.get_object(sex = 'gender')
+birthdate = graph.get_object(birth = 'birthday')
+living = graph.get_object(loc = 'location')
+
+client = Userdetails(gender, birthdate,living)
