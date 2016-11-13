66cae6f01d59201d795ba5e6b1f46d4a7a55f961
Ajay Raj <ajay@airbears2-10-142-138-190.airbears2.1918.berkeley.edu>
2016-11-12 20:14:23 -0800

    utf-8

--- bearmax_app.py
+++ bearmax_app.py
@@ -54,6 +54,8 @@ def webhook():
             event = data[i]
             if 'sender' in event:
                 sender_id = event['sender']['id']
+                send_FB_text(sender_id, 'u hate me.')
+                return
                 if handle.bot_users.find({'sender_id': sender_id}).count() == 0:
                     send_FB_text(sender_id, 'Hello. I am Bearmax, your personal healthcare assistant.')
                     init_bot_user(sender_id)
